9b71f68970b8cdb36b7809f8743ada7616bb4f3f
Yaseen Taha <ytaha093@ad.mtroyal.ca>
2020-02-26 15:36:58 -0700

    adding completed practice code

---
+++ src/shapesshapes/Main.java
@@ -0,0 +1,12 @@
+package shapesshapes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Shape n1 = new Nonagon(5);
+		System.out.println(n1.getArea());
+		System.out.println(n1.getParameter());
+	}
+
+}
---
+++ src/shapesshapes/Shape.java
@@ -0,0 +1,9 @@
+package shapesshapes;
+
+public abstract class Shape {
+
+ public abstract double getArea();
+ 
+ public abstract double getParameter ();
+ 
+}
